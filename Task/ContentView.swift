70095fa1dc94ff1ea6e78e7170f0cfa5fe25e3f1
hizaoni <hizaoni@yahoo.co.jp>
2022-06-07 11:58:06 +0900

    Initial Commit

---
+++ Task/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Task
+//
+//  Created by 高本直矢 on 2022/06/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Task/TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskApp.swift
+//  Task
+//
+//  Created by 高本直矢 on 2022/06/07.
+//
+
+import SwiftUI
+
+@main
+struct TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
